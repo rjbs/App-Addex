0ddab8c92b4964fd5d6716269e235033fce4c998
Ricardo SIGNES <rjbs@codesimply.com>
2007-05-12 00:32:42 +0000

    r31633@knight:  rjbs | 2007-05-11 19:45:59 -0400  prep release

--- lib/App/Addex.pm
+++ lib/App/Addex.pm
@@ -12,13 +12,13 @@ App::Addex - generate mail tool configuration from an address book
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
   $Id$
 
 =cut
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 =head1 DESCRIPTION
 
