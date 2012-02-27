457ae743907f25e978cfba94cc1d3c7645fd9774
Ricardo Signes <rjbs@cpan.org>
2012-02-27 13:29:35 -0500

    sort addr entries at least a little

--- lib/App/Addex.pm
+++ lib/App/Addex.pm
@@ -147,7 +147,7 @@ future, as we really want an iterator, not just a list.
 
 sub entries {
   my ($self) = @_;
-  return $self->addressbook->entries;
+  return sort { $a->name cmp $b->name } $self->addressbook->entries;
 }
 
 =head2 run
